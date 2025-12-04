622d8202960f1c01cfc78c996f70db480d98082b
Israel Brito <israel.olv05@gmail.com>
2025-12-04 19:31:54 -0300

    Deploy web build corrigido

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "751352226"
+    serviceWorkerVersion: "3757983654"
   }
 });
